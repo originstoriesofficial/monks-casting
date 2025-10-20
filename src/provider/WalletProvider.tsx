0c887c89379b1fe19881c5a67d301d25af25ee57
sean <playoriginstory@gmail.com>
2025-10-20 13:31:42 +0200

    fix

--- src/provider/WalletProvider.tsx
+++ src/provider/WalletProvider.tsx
@@ -1,40 +1,44 @@
-'use client'
+'use client';
 
-import { WagmiProvider, http } from 'wagmi'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ConnectKitProvider } from "connectkit";
-import { RainbowKitProvider, getDefaultConfig, darkTheme } from '@rainbow-me/rainbowkit'
-import '@rainbow-me/rainbowkit/styles.css'
+import { WagmiProvider, createConfig, http } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit';
+import { ConnectKitProvider } from 'connectkit';
+import { injected, walletConnect, coinbaseWallet } from 'wagmi/connectors';
 import { mantle } from 'viem/chains';
+import '@rainbow-me/rainbowkit/styles.css';
 
+const config = createConfig({
+  chains: [mantle],
+  transports: {
+    [mantle.id]: http(),
+  },
+  connectors: [
+    injected(),
+    walletConnect({ projectId: '066465a4f5d400c9eccad76612f98c5a' }),
+    coinbaseWallet({ appName: 'MonksCasting' }),
+  ],
+});
 
-export const config = getDefaultConfig({
-    appName: 'MonksCasting',
-    projectId: '066465a4f5d400c9eccad76612f98c5a', 
-    chains: [mantle],
-    transports: {
-
-        // [mantleSepoliaTestnet.id]: http(),
-        [mantle.id]:http()
-    }
-})
-
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    return (
-        <WagmiProvider config={config}>
-            <QueryClientProvider client={queryClient}>
-                <RainbowKitProvider theme={darkTheme({
-                    accentColor: "#deccaf",
-                    accentColorForeground: "black",
-                    borderRadius: "medium",
-                    fontStack: "system",
-                    overlayBlur: "small",
-                })} initialChain={mantle}>
-                    <ConnectKitProvider>{children}</ConnectKitProvider>
-                </RainbowKitProvider>
-            </QueryClientProvider>
-        </WagmiProvider>
-    )
-} 
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider
+          theme={darkTheme({
+            accentColor: '#deccaf',
+            accentColorForeground: 'black',
+            borderRadius: 'medium',
+            fontStack: 'system',
+            overlayBlur: 'small',
+          })}
+          initialChain={mantle}
+        >
+          <ConnectKitProvider>{children}</ConnectKitProvider>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
